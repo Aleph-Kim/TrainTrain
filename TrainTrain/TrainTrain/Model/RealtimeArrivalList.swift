ca5f4124e7e7981ac080b89ed3d9a07820784d6e
Jager-yoo <y73447jh@gmail.com>
2022-09-11 22:43:20 +0900

    style: 모델 타입 주석에서 Note 표시 제거

--- TrainTrain/TrainTrain/Model/RealtimeArrivalList.swift
+++ TrainTrain/TrainTrain/Model/RealtimeArrivalList.swift
@@ -2,33 +2,33 @@ import Foundation
 
 struct RealtimeArrivalList: Decodable {
 
-  /// - Note: 지하철 호선 ID
+  /// 지하철 호선 ID
   let subwayLineID: String
-  /// - Note: 도착지 방면 (성수행 - 구로디지털단지방면)
+  /// 도착지 방면 (성수행 - 구로디지털단지방면)
   let trainDestination: String
-  /// - Note: 이전 지하철역 ID
+  /// 이전 지하철역 ID
   let previousStationID: String
-  /// - Note: 다음 지하철역 ID
+  /// 다음 지하철역 ID
   let nextStationID: String
-  /// - Note: 지하철역 ID
+  /// 지하철역 ID
   let stationID: String
-  /// - Note: 지하철역명
+  /// 지하철역명
   let stationName: String
-  /// - Note: 열차 종류 (급행, ITX) - 일반 지하철인 경우, nil 나옵니다.
+  /// 열차 종류 (급행, ITX) - 일반 지하철인 경우, nil 나옵니다.
   let trainType: TrainType
-  /// - Note: 열차 도착 예정 시간 (단위:초) - Estimated Time of Arrival
+  /// 열차 도착 예정 시간 (단위:초) - Estimated Time of Arrival
   let eta: String
-  /// - Note: 종착 지하철역ID
+  /// 종착 지하철역ID
   let terminusStationID: String
-  /// - Note: 종착 지하철역명
+  /// 종착 지하철역명
   let terminusStationName: String
-  /// - Note: 열차 도착정보를 생성한 시각
+  /// 열차 도착정보를 생성한 시각
   let createdAt: String
-  /// - Note: 첫 번째 도착 메세지 (전역 진입, 전역 도착 등)
+  /// 첫 번째 도착 메세지 (전역 진입, 전역 도착 등)
   let firstMessage: String
-  /// - Note: 두 번째 도착 메세지 (종합운동장 도착, 12분 후 (광명사거리) 등)
+  /// 두 번째 도착 메세지 (종합운동장 도착, 12분 후 (광명사거리) 등)
   let secondMessage: String
-  /// - Note: 도착코드 (0:진입, 1:도착, 2:출발, 3:전역출발, 4:전역진입, 5:전역도착, 99:운행중)
+  /// 도착코드 (0:진입, 1:도착, 2:출발, 3:전역출발, 4:전역진입, 5:전역도착, 99:운행중)
   let arrivalCode: String
 
   init(from decoder: Decoder) throws {
--- TrainTrain/TrainTrain/Model/TrainInfo.swift
+++ TrainTrain/TrainTrain/Model/TrainInfo.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct TrainInfo: Decodable {
 
-  /// - Note: 통신 결과에 대한 각종 상태값과 조회 가능한 전체 열차의 개수가 담겨있습니다.
+  /// 통신 결과에 대한 각종 상태값과 조회 가능한 전체 열차의 개수가 담겨있습니다.
   let errorMessage: ErrorMessage
-  /// - Note: 특정 지하철역에 대한 실시간 도착정보가 배열 형태로 담겨있습니다.
+  /// 특정 지하철역에 대한 실시간 도착정보가 배열 형태로 담겨있습니다.
   let realtimeArrivalList: [RealtimeArrivalList]
 }
