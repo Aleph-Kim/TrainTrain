2f062c858fd981cea3d32f575f4bf4dc3100772a
Coda <coda@yagom.net>
2022-09-11 14:28:41 +0900

    docs: Project file 추가 및 기반 설정

---
+++ TrainTrain/TrainTrain/TrainTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainTrainApp.swift
+//  TrainTrain
+//
+//  Created by coda on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct TrainTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
