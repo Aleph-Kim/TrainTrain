eddffe985b45f5a0bed08bae07de4b3fd83129d5
Jager-yoo <y73447jh@gmail.com>
2022-10-02 17:04:42 +0900

    feat: PreSelection 페이지 추가, gitignore 수정(DS_Store 거부)

--- TrainTrain/TrainTrain/Presentation/View/SelectionView.swift
+++ TrainTrain/TrainTrain/Presentation/View/SelectionView.swift
@@ -9,9 +9,14 @@ struct SelectionView: View {
   // MARK: - body
   var body: some View {
     TabView(selection: $selectionStep) {
-      page1
-      page2
-      page3
+      page1.tag(SelectionStep.preSelection)
+//        .gesture(DragGesture())
+      page2.tag(SelectionStep.lineNumber)
+//        .gesture(DragGesture())
+      page3.tag(SelectionStep.station)
+//        .gesture(DragGesture())
+      page4.tag(SelectionStep.direction)
+//        .gesture(DragGesture())
     }
     .tabViewStyle(.page(indexDisplayMode: .never))
     .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height * 0.4) // 화면 높이의 40% 사용
@@ -19,6 +24,34 @@ struct SelectionView: View {
 
   // MARK: - page1
   var page1: some View {
+    VStack(spacing: 10) {
+      HStack {
+        Text("Pre Selection Page")
+          .bold()
+          .padding(.horizontal, 16)
+          .padding(.vertical, 10)
+          .background(.quaternary)
+          .clipShape(Capsule())
+        Spacer()
+      }
+
+      VStack {
+        Button("시작하기") {
+          withAnimation {
+            selectionStep = .lineNumber
+          }
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(.quaternary)
+//      .background(selectedLine?.color)
+      .cornerRadius(20)
+    }
+    .padding(.horizontal)
+  }
+
+  // MARK: - page2
+  var page2: some View {
     VStack(spacing: 10) {
       HStack {
         Text("몇 호선 인가요?")
@@ -32,7 +65,6 @@ struct SelectionView: View {
 
       ScrollView(showsIndicators: false) {
         VStack(alignment: .leading, spacing: 20) {
-          Spacer(minLength: 5)
           ForEach(SubwayLine.allCases) { line in
             Button {
               withAnimation {
@@ -57,19 +89,18 @@ struct SelectionView: View {
                 .padding(.leading, 20)
             }
           }
-          Spacer(minLength: 5)
         }
-        .frame(maxWidth: .infinity)
+        .padding(.vertical, 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
       }
       .background(.quaternary)
       .cornerRadius(20)
     }
-    .tag(SelectionStep.lineNumber)
     .padding(.horizontal)
   }
 
-  // MARK: - page2
-  var page2: some View {
+  // MARK: - page3
+  var page3: some View {
     VStack(spacing: 10) {
       HStack {
         if let selectedLine = selectedLine {
@@ -94,23 +125,19 @@ struct SelectionView: View {
       }
 
       VStack {
-        VStack {
-          Spacer()
-        }
-        .frame(maxWidth: .infinity)
+        Spacer()
       }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
       .background(.quaternary)
       .background(selectedLine?.color)
       .cornerRadius(20)
     }
-    .tag(SelectionStep.station)
     .padding(.horizontal)
   }
 
-  // MARK: - page3
-  var page3: some View {
-    Text("Page 3")
-      .tag(SelectionStep.direction)
+  // MARK: - page4
+  var page4: some View {
+    Text("Page 4")
   }
 }
 
@@ -119,6 +146,16 @@ fileprivate enum SelectionStep {
   case lineNumber
   case station
   case direction
+}
+
+extension View {
+  @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+    if condition {
+      transform(self)
+    } else {
+      self
+    }
+  }
 }
 
 struct SelectionView_Previews: PreviewProvider {
