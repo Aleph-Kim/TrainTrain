1e8871b67849b92abe8a6b0cd93c21a3b558a2ec
coda <codable.space@gmail.com>
2022-11-20 16:58:25 +0900

    fix: 지나친 열차 제거 로직 수정

--- TrainTrain/TrainTrain/Presentation/View/NewArrivalView.swift
+++ TrainTrain/TrainTrain/Presentation/View/NewArrivalView.swift
@@ -81,7 +81,7 @@ struct NewArrivalView: View {
           directionStationID: directionStationID)
         
         for newTrainInfo in newTrainInfos {
-          if !trainInfos.contains(where: { $0.id == newTrainInfo.id && newTrainInfo.secondMessage != selectedStationInfo.stationName }) {
+          if !trainInfos.contains(where: { $0.id == newTrainInfo.id || newTrainInfo.secondMessage != selectedStationInfo.stationName }) {
             trainInfos.append(newTrainInfo)
           }
         }
